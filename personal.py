f3b8a186d6c4209db95667718eb1266fcc0e6337
student <student@info-23.cshl.edu>
2022-10-11 11:04:27 -0400

    added color, activity and animal

--- personal.py
+++ personal.py
@@ -1,3 +1,18 @@
 #! /usr/bin/env python3 
+import sys
+
+name1 = sys.argv[1]
+color1 = sys.argv[2]
+activity = sys.argv[3]
+animal = sys.argv[4]
+print(name1, color1, activity, animal) 
+
 name = 'Elaine'
-print ("My name:" + ''  + name)
+print ("My name:" ,  name)
+color ='Teal'
+print ('My favorite color:' ,  color)
+fav_act = 'Games'
+print ('My favorite activity:' , fav_act)
+fav_animal = 'Dolphin'
+print ('My favorite animal:' , fav_animal)
+
