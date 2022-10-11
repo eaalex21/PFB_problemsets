2aaff54029ddab4116b01486f287cb8f20f3a663
student <student@info-23.cshl.edu>
2022-10-11 15:41:16 -0400

    adding if/else part 2

--- python2_problems/python2_practice.py
+++ python2_problems/python2_practice.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 practice = 12
-print(practice)
+print(practice) # check to see if creating variable worked
 if practice > 50:
   print('True') 
 else:
   print('Not True')
+
+#second if/else
+x = -86
+print(x) # check variable made 
+if x >= 0:
+  print ('Positive')
+else:
+  print('Negative')
+
