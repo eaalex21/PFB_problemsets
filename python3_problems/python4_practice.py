6434bf409f6ba9442ac127cfbb9c128739770f43
student <student@info-23.cshl.edu>
2022-10-12 20:59:25 -0400

    finished this py

--- python3_problems/python4_practice.py
+++ python3_problems/python4_practice.py
@@ -12,3 +12,53 @@ species[1]
 type(species) # check type of species
 sorted(species, key = str.lower)
 sorted(species, key = len)
+
+# using of while loops
+hundred = 1
+while hundred <= 100:
+	print(hundred)
+	hundred += 1
+print('Done')
+
+fac = 1
+i = 1 
+while i <= 1000:
+	fac =  fac * 1
+	i = i + 1
+num = 1000
+count = 1
+factorial = 1
+
+while count <=1000:
+	factorial + factorial*count
+	count = count+1
+print(factorial)
+
+# iterate through list
+list1 = [101, 2, 15, 22, 95, 33, 2, 27, 72, 15, 52]
+list1_it = iter(list1)
+for num in list1_it:
+  if num%2 == 0:
+    print(num)
+sume = 0
+sumo = 0
+list1sorted = sorted(list1)
+list1sorted_it = iter(list1)
+for num in list1sorted_it:
+	if num%2 == 0:
+		sume += num
+	else:
+		sumo += num
+	print(num)
+print(sume)
+print(sumo)
+
+# range 0-99
+for number in range(100):
+	print(number)
+
+hundred = [number + 1 for number in range(100)]
+print(hundred)
+
+
+ 
