7fbe28d5a37a82a6d2e623ac3c1b2394c1eacab9
student <student@info-23.cshl.edu>
2022-10-13 13:26:57 -0400

    adding input method

--- python3_problems/dict_practice.py
+++ python3_problems/dict_practice.py
@@ -15,6 +15,15 @@ print(favs)
 favs_things = 'organism'
 print(favs[favs_things])
 
-str = input('key options')
-print("Here are your options:", str)
+# using input f(x)
+favs_thing = 'book\tsong\ttree\torganism'
+favs_st = input("What is your favorite: " + favs_thing)
+print("Here are your options: ", favs_st)
 
+favs_thing = 'song'
+favs_st = input("What is your favorite " + favs_thing)
+print("This is your favorite song:",favs_st)
+
+#use for loop to call from dict
+for things in favs:
+	print(things, favs[things])
