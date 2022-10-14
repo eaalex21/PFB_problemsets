7e3761fcde07b7e229974136562c2a3ce2ffa935
student <student@info-23.cshl.edu>
2022-10-14 11:31:13 -0400

    switching nobody with name

--- python3_problems/python07_practice.py
+++ python3_problems/python07_practice.py
@@ -5,5 +5,18 @@
 import re
 
 # nobody
-py7no = open("Python_07_nobody.txt", "r")
-for 
+with open("Python_07_nobody.txt", "r") as py7no_read:
+	for line in py7no_read:
+		line = line.rstrip()
+		for nobody in  re.finditer(r'Nobody', line):
+			n1 = nobody.group(0)
+			position = nobody.start(0) + 1
+			print(n1, position)
+
+# nobody switch with hendrix
+
+
+
+
+
+			
